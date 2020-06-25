4118d88eb03e4a0072ddff57c8a1a4555475f999
sachinmalik <sachin.15085@ggnindia.dronacharya.info>
2020-06-25 23:06:03 +0530

    stepview

---
+++ app/src/main/java/com/samapps/restituo/ui/view/DashBoardActivity.java
@@ -0,0 +1,33 @@
+package com.samapps.restituo.ui.view;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import com.samapps.restituo.R;
+
+public class DashBoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(DashBoardActivity.this,CreateRestituoActivity.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/samapps/restituo/ui/view/SplashActivity.java
@@ -0,0 +1,42 @@
+package com.samapps.restituo.ui.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.samapps.restituo.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        if(FirebaseAuth.getInstance().getCurrentUser() == null) {
+            // Start sign in/sign up activity
+            startActivity(new Intent(this,LoginActivity.class));
+            finish();
+        } else {
+            // User is already signed in. Therefore, display
+            // a welcome Toast
+            Toast.makeText(this,
+                    "Welcome " + FirebaseAuth.getInstance()
+                            .getCurrentUser()
+                            .getPhoneNumber(),
+                    Toast.LENGTH_LONG)
+                    .show();
+
+            startActivity(new Intent(this,DashBoardActivity.class));
+            finish();
+
+        }
+    }
+
+
+
+
+}
