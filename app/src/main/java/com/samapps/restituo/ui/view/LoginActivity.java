3bc08244bc16d9b0c461559e66afb17b09ad4b22
Jyoti-Nigam <jyoti28.nigam@gmail.com>
2021-01-28 11:19:33 +0530

    Country code done

--- app/src/main/java/com/samapps/restituo/ui/view/LoginActivity.java
+++ app/src/main/java/com/samapps/restituo/ui/view/LoginActivity.java
@@ -11,6 +11,7 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.hbb20.CountryCodePicker;
 import com.samapps.restituo.R;
 
 public class LoginActivity extends AppCompatActivity {
@@ -19,17 +20,19 @@ public class LoginActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
         final EditText usernameEditText = findViewById(R.id.username);
-       // final EditText passwordEditText = findViewById(R.id.password);
         final Button loginButton = findViewById(R.id.login);
         final ProgressBar loadingProgressBar = findViewById(R.id.loading);
+        final CountryCodePicker et1 = findViewById(R.id.et1);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                final  String mob=et1.getSelectedCountryCodeWithPlus().concat(usernameEditText.getText().toString());
+                Toast.makeText(getApplicationContext(),mob,Toast.LENGTH_LONG).show();
+
                 loadingProgressBar.setVisibility(View.VISIBLE);
-                startActivity(new Intent(LoginActivity.this,VerifyPhoneActivity.class).putExtra("mobile",usernameEditText.getText().toString()));
+                startActivity(new Intent(LoginActivity.this,VerifyPhoneActivity.class).putExtra("mobile",mob));
             }
         });
     }
